d76e7fde1ffb477cb084623061d6c355568b2695
huangsheng <huangsheng@nuls.io>
2020-01-19 10:01:32 +0800

    开发环境添加可通过ip访问

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -2,6 +2,9 @@ const path = require("path");
 const merge = require("webpack-merge");
 const webpack = require("webpack");
 const commonConfig = require("./webpack.base.config.js");
+const openBrowserPlugin = require("open-browser-webpack-plugin");
+
+const POST = process.env.PORT || 8882
 
 module.exports = merge(commonConfig, {
   mode: "development",
@@ -16,7 +19,9 @@ module.exports = merge(commonConfig, {
   devServer: {
     hot: true,
     // contentBase: path.resolve(__dirname, "../dist"), //告诉服务(dev server)在哪里查找文件，默认不指定会在是当期项目根目录
-    port: 8882,
+    port: POST,
+    host: '0.0.0.0', //设置可以通过本机IP访问
+    //open: true, //编译完成自动打开页面
     progress: true,
     // color: true,//只能在cli中添加
     historyApiFallback: true, //将所有的404都连接到index.html,react使用historyRouter时必须设置，上线后需在服务器配置把非/的请求rewrite到/去
@@ -24,7 +29,6 @@ module.exports = merge(commonConfig, {
       //当出现编译器错误或警告时，就在网页上显示一层黑色的背景层和错误信息
       errors: true
     },
-    open: true, //编译完成自动打开页面
     proxy: {
       // 代理到后端的服务地址
       "/api": {
@@ -36,6 +40,7 @@ module.exports = merge(commonConfig, {
   },
   plugins: [
     //开启HMR(热替换功能,替换更新部分,不重载页面！) devServer中设置hot后该插件自动开启
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new openBrowserPlugin({url: "http://127.0.0.1:" + POST})
   ]
 });
